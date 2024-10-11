dbcb2d0a17d0780534e22ed06aa866ae5cf1bfec
Israel Tiburcio <agenor.it@gmail.com>
2024-10-11 15:43:33 -0600

    commit nuevo proyecto

---
+++ VistasCustomizablesUI/VistasCustomizablesUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VistasCustomizablesUIApp.swift
+//  VistasCustomizablesUI
+//
+//  Created by IsraelTiburcio on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VistasCustomizablesUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
